4dad81a94830a291f26cb93710f29412ed398618
Muhammad.umer <mu644505@gmail.com>
2026-09-25 07:20:49 -0700

    Allow local login testing

--- server.js
+++ server.js
@@ -49,6 +49,8 @@ const allowedOrigins = new Set([
   "http://127.0.0.1:3000",
   "http://localhost:5500",
   "http://127.0.0.1:5500"
+    "http://localhost:5502",
+  "http://127.0.0.1:5502"
 ]);
 
 app.use(
